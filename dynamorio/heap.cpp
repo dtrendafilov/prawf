c0b3f225b7c1f1df987157356ef0af8433e1d80c
Dimitar Trendafilov <trendafilov.dn@gmail.com>
2023-08-12 17:10:04 +0300

    Add Dr Memory heap error checks

---
+++ dynamorio/heap.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <cstdlib>
+#include <drmemory_annotations.h>
+
+void unaddressable_write(int* p)
+{
+    p[4] = 2;
+}
+
+void corrupt(int* p)
+{
+    p[2] = 4;
+    p[4] = 2;
+}
+
+int main()
+{
+    {
+        auto undef = new int;
+        unaddressable_write(undef);
+        auto answer = new int(*undef + 42);
+        std::printf("The answer is %d\n", *answer);
+    }
+    {
+        auto ints = new int[16];
+        DRMEMORY_ANNOTATE_MAKE_UNADDRESSABLE(ints + 4, sizeof(int));
+        corrupt(ints);
+        delete [] ints;
+    }
+    {
+        auto memory = std::malloc(64);
+        DRMEMORY_ANNOTATE_MAKE_UNADDRESSABLE(memory, 64);
+        *((int*)(memory) + 4) = 42;
+        std::free(memory);
+    }
+    return 0;
+}
